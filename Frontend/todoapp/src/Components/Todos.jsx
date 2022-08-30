3e6b2248e2a32a4d15fbdff4a808f5f4b43cb02c
lawandepranav <lawandepranav@gmail.com>
2022-08-30 12:03:46 +0530

    commit

--- Frontend/todoapp/src/Components/Todos.jsx
+++ Frontend/todoapp/src/Components/Todos.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { useEffect } from 'react'
 import { useState } from 'react'
+import axios from 'axios'
 
 const Todo = () => {
 
@@ -21,8 +22,28 @@ const Todo = () => {
         }
     }
 
-    const handleAdd=()=>{
-     
+    const handleAdd=(e)=>{
+        
+            e.preventDefault();
+            const data = {
+            title,
+            status,
+            lable
+          };
+        
+        axios({
+            method: 'post',
+            url: `http://localhost:3030/todos`,
+            data: data,
+        })
+        .then(function (response) {
+            console.log(response);
+        })
+        .catch(function (error) {
+            console.log(error);
+        });
+          
+        
     }
     const handleDelete=()=>{
      
@@ -43,7 +64,7 @@ const Todo = () => {
     <input type="text" placeholder='Title' onChange={(e)=>setTitle(e.target.value)} />
       <input type="checkbox" placeholder='Status' onChange={(e)=>setStatus(e.target.value)} />
       <input type="text" placeholder='Lable' onChange={(e)=>setLable(e.target.value)} />
-      <button onClick={()=>handleAdd()}>Add Todo</button>
+      <button onClick={handleAdd}>Add Todo</button>
     </div>
     <div>
    {
@@ -53,6 +74,8 @@ const Todo = () => {
             <>
             <h4>{ele.createdAt}</h4>
             <h4>{ele.updatedAt}</h4>
+            <h4>{ele._id}</h4>
+          
             <button onClick={()=>handleDelete()}>Delete</button>
             <button onClick={()=>handleUpdate()}>Update</button>
             </>
